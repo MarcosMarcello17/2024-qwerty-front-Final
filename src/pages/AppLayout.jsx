c1d8a42b074d167d216fc43d288805e319263e5b
Marcos <marcosmarcello17@gmail.com>
2026-08-19 20:53:12 -0300

    Add pendingTransactions to Layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -4,6 +4,10 @@ import Header from "./components/Header";
 import { SidebarNav } from "./components/SidebarNav";
 import { useAuth } from "@/hooks/useAuth";
 import { AuthenticationExpiredNotification } from "@/components/AuthenticationExpiredNotification";
+import AlertPending from "./components/AlertPending";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getPersonalCategorias } from "@/functions/getPersonalCategorias";
 
 export default function AppLayout({ children }) {
   // Use the authentication hook to constantly verify token validity
@@ -13,6 +17,31 @@ export default function AppLayout({ children }) {
     showAuthExpiredNotification,
     setShowAuthExpiredNotification,
   } = useAuth();
+  const [pendTran, setPendTran] = useState(false);
+  const [tranPendiente, setTranPendiente] = useState({});
+  const { data: payCategories } = useQuery(getPersonalCategorias());
+  const isAccepted = async (transaction, categoria, tipoGasto) => {
+    await aceptarTransaccion(transaction, categoria, tipoGasto);
+    eliminarTransaccionPendiente(transaction.id);
+    if (
+      transaction.id_reserva !== "Cobro" &&
+      transaction.id_reserva !== "Pago"
+    ) {
+      enviarRespuesta("aceptada", transaction.id_reserva);
+    }
+    setPendTran(false);
+  };
+
+  const isRejected = (transaction) => {
+    eliminarTransaccionPendiente(transaction.id);
+    if (
+      transaction.id_reserva !== "Cobro" &&
+      transaction.id_reserva !== "Pago"
+    ) {
+      enviarRespuesta("rechazada", transaction.id_reserva);
+    }
+    setPendTran(false);
+  };
   return (
     <SidebarProvider defaultOpen={true}>
       <div className="flex min-h-full min-w-full flex-col">
@@ -30,7 +59,16 @@ export default function AppLayout({ children }) {
             <SidebarNav />
             <div className="flex flex-1 flex-col transition-[padding-left] duration-200 ease-linear md:pl-(--sidebar-width) group-data-[collapsible=icon]/sidebar-wrapper:md:pl-(--sidebar-width-icon)">
               <Header />
-              <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
+              <main className="flex-1 p-4 sm:p-6 lg:p-8">
+                {children}
+                <AlertPending
+                  isOpen={pendTran}
+                  pendingTransaction={tranPendiente}
+                  isAccepted={isAccepted}
+                  isRejected={isRejected}
+                  payCategories={payCategories}
+                />
+              </main>
             </div>
           </>
         ) : null}
